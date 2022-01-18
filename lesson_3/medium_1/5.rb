6d862162816233d2fdbd9512bf32af45fa45689a
Kurt Kerns <kurtkerns20@gmail.com>
2022-01-18 15:47:15 -0600

    subtle changes in lesson_3 exercises

--- lesson_3/medium_1/5.rb
+++ lesson_3/medium_1/5.rb
@@ -1,3 +1,20 @@
+# Fix this
+# limit = 15
+
+# def fib(first_num, second_num)
+#   while first_num + second_num < limit
+#     sum = first_num + second_num
+#     first_num = second_num
+#     second_num = sum
+#   end
+#   sum
+# end
+
+# result = fib(0, 1)
+# puts "result is #{result}"
+
+# limit is defined outside of the scope of fib,
+# making it an additional argument would allow fib to pass it to fib
 def fib(first_num, second_num, limit)
   while first_num + second_num < limit
     sum = first_num + second_num
