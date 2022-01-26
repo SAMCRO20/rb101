c13a9aeca78fbd4532366d4461a446f604952b30
Kurt Kerns <kurtkerns20@gmail.com>
2022-01-26 12:25:58 -0600

    finished with calc refact and yml link

--- lesson_2/mortgage_calc_refact.rb
+++ lesson_2/mortgage_calc_refact.rb
@@ -1,9 +1,18 @@
 # mortgage_calc_refact.rb
 
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calc_refact.yml')
+
 YES_OR_NO = ['y', 'n', 'yes', 'no']
 
-def prompt(string)
-  puts ">> #{string}"
+def messages(message)
+  MESSAGES[message]
+end
+
+def prompt(key)
+  message = messages(key)
+
+  puts ">> #{message}"
 end
 
 def clear_screen
@@ -22,63 +31,46 @@ end
 
 clear_screen
 
-prompt("Hey there! Welcome to the Mortgage Calculator!")
-prompt("What is your name?")
+prompt(:welcome)
+prompt(:name)
 name = gets.chomp
 
 until valid_name?(name)
   clear_screen
 
-  prompt("It looks like you didn't type anything. Please enter a name.")
+  prompt(:valid_name)
   name = gets.chomp
 end
 
 clear_screen
 
-prompt <<~MSG
-         Welcome, #{name}. I will need to collect some information
-            from you to calculate the monthly payment of your loan.
-       MSG
-sleep(5)
-
-clear_screen
+puts ">> Okay, #{name}!"
+prompt(:getting_started)
+sleep 2
 
 loop do
-  prompt("What is the loan amount?")
-  print("$")
+  prompt(:loan_amount)
+  print("$ ")
   loan_amount = gets.chomp
 
   until valid_number?(loan_amount)
-    clear_screen
 
-    prompt("That doesn't look like a valid number." \
-           " Please enter a valid loan amount.")
-    print("$")
+    prompt(:valid_loan_amount)
+    print("$ ")
     loan_amount = gets.chomp
   end
 
-  clear_screen
-
-  prompt("What is your loan duration (in years)?")
+  prompt(:loan_duration)
   loan_duration_in_years = gets.chomp
 
   until valid_number?(loan_duration_in_years)
-    clear_screen
 
-    prompt("That doesn't look quite right." \
-           " Please enter a valid loan duration (in years).")
+    prompt(:valid_loan_duration)
     loan_duration_in_years = gets.chomp
   end
 
-  clear_screen
-
-  prompt <<~MSG
-           What is the loan APR?
-                (Examples: [1 = 1%] [5.5 = 5.5%] [9.99 = 9.99%])
-        MSG
+  prompt(:apr)
   yearly_apr_as_percent = gets.chomp
-
-  clear_screen
 
   duration_in_months = loan_duration_in_years.to_f * 12
   monthly_apr = yearly_apr_as_percent.to_f / 100 / 12
@@ -88,20 +80,19 @@ loop do
                     (1 - (1 + monthly_apr)**(-duration_in_months)))
 
   output_string = "The monthly payment is $#{format('%.2f', monthly_payment)}"
-  prompt(output_string)
+  puts ">> #{output_string}"
   sleep(2)
   puts("-----------------------------------------")
 
-  prompt("Would you like to perform another calculation? [y/n]")
+  prompt(:again)
   repeat_calculation = gets.chomp.downcase
 
   until YES_OR_NO.include?(repeat_calculation)
     clear_screen
 
-    prompt(output_string)
+    puts ">> #{output_string}"
     puts("-----------------------------------------")
-    prompt("It seems like you didn't give a valid response.\n" \
-           ">> Would you like to perform another calculation? [y/n]")
+    prompt(:valid_again)
     repeat_calculation = gets.chomp.downcase
   end
 
@@ -110,4 +101,4 @@ loop do
   break if repeat_calculation.start_with?('n')
 end
 
-prompt("Thank you for using the Mortgage Calculator. Goodbye, #{name}!")
+puts "Thank you for using the Mortgage Calculator. Goodbye, #{name}!"
